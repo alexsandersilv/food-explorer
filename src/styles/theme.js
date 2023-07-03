9ef914ffddab76eaf6d48a23ce8f88e1a430df3f
Alexsander <81785712+alexsandersilv@users.noreply.github.com>
2023-07-03 12:14:16 -0300

    Update theme.js

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -2,23 +2,9 @@ export default {
   COLORS: {
     WHITE: "#FFFFFF",
     GRAY: "#E1E1E6",
-    
     INPUT: "#7C7C8A",
-
     FOOTER: "rgba(255, 255, 255, 0.3)",
-    GRAY_100: "#C4C4CC", /*É a cor do texto das descrições*/
-    GRAY_400: "rgba(255, 255, 255, 0.05)", /*É a cor dos botões de Pix e Cartão*/
-
-    RED: "#FF002E", /*É a cor do ícone para apagar marcadores*/
-    BLACK: "#0D0C0F", /*É a cor preta*/
-    FULL_BLACK: "#000000", /*É a cor 100% preta*/
-
-    BLUE: "#0099ff", /*É a cor do logo e dos textos rosas/azuis*/
-    BLUE_100: "#82F3FF", /*É a cor dos preços nos produtos*/
-    BLUE_300: "#192227", /*É a cor das bordas das tabelas*/
-    BLUE_400: "#091E26", /*É a 1ª cor de fundo do gradiente na Home*/
-    BLUE_500: "#00131C", /*É a 2ª cor de fundo do gradiente na Home*/
-
+    
   },
   BACKGROUND: {
     PAGE: "#000A0F",
